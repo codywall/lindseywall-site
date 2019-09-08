3cb5e91fd6262f04b0fd970354f917bd1f3f66b9
codywall <cody.wall@gmail.com>
2019-09-08 15:43:28 -0700

    Fixed nav and layout

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    //Navigation 
+    //NAVIGATION SCRIPTS
     const toggleButton = document.getElementById("menu-toggle");
     const mobileNav = document.getElementById("mobile-nav");
     const topNav = document.getElementsByTagName("nav");
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const navModal = document.getElementById("nav__modal");
     const menuItems = document.getElementsByClassName("nav__link");
 
+    //toggle modal which covers the background
     function toggleModal() {
         if (navModal.className === "open") {
             navModal.className = "closed";
@@ -16,43 +17,33 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    //toggle displaying the mobile navigation
     function toggleNav() {
         mobileNav.classList.toggle("active");
         topNav[0].classList.toggle("hidden__nav");
         toggleModal();
     }
 
-    //added event listeners to buttons
+    //add event listeners to buttons
     toggleButton.addEventListener('click', toggleNav);
     closeButton.addEventListener('click', toggleNav);
-
+    navModal.addEventListener('click', toggleNav, false);
     for (var i = 0, len = menuItems.length; i < len; i++) {
         menuItems[i].addEventListener('click', toggleNav);
     }
 
-    document.getElementById('nav__modal').addEventListener('click', toggleNav, false);
 
-
-    // function handleTouch(e) {
-    //     var x = e.changedTouches[0].clientX;
-    //     var total = this.clientWidth;
-    //     var position = x - total;
-    //     if (position < 0) this.style.left = (x - total) + 'px'
-    //     else if (position >= 0) this.style.left = 0 + 'px'
-    // }
-
-    // function handleTouchEnd(e) {
-    //     var x = e.changedTouches[0].clientX;
-    //     var total = this.clientWidth;
-    //     var position = x - total;
-    //     this.style.left = "";
-    //     if (position <= -total * 0.5) { closeNav(); }
-    // }
-
-    // mobileNav.addEventListener('touchstart', handleTouch, false)
-    // mobileNav.addEventListener('touchmove', handleTouch, false)
-    // mobileNav.addEventListener('touchend', handleTouchEnd, false)
-
+    //hide menu on scroll down
+    let prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("nav").style.top = "0";
+        } else {
+            document.getElementById("nav").style.top = "-100vw";
+        }
+        prevScrollpos = currentScrollPos;
+    }
 
 }, false);
 
