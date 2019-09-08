919d8bdec4bd6fadb8aa1a10f9f5a34ad47cf1c2
codywall <cody.wall@gmail.com>
2019-09-08 15:10:41 -0700

    Fixed desktop layout

--- main.js
+++ main.js
@@ -1,64 +1,57 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    //Navigation 
     const toggleButton = document.getElementById("menu-toggle");
-    const topNav = document.getElementById("mobile-nav");
-    const closeButton = document.getElementById("close__button");
     const mobileNav = document.getElementById("mobile-nav");
+    const topNav = document.getElementsByTagName("nav");
+    const closeButton = document.getElementById("close__button");
     const navModal = document.getElementById("nav__modal");
     const menuItems = document.getElementsByClassName("nav__link");
 
     function toggleModal() {
-        if (navModal.className === "open")
+        if (navModal.className === "open") {
             navModal.className = "closed";
-        else
+        } else {
             navModal.className = "open";
+        }
     }
 
-    function closeNav() {
-        topNav.classList.toggle("active");
+    function toggleNav() {
+        mobileNav.classList.toggle("active");
+        topNav[0].classList.toggle("hidden__nav");
         toggleModal();
     }
 
     //added event listeners to buttons
-    toggleButton.addEventListener('click', closeNav);
-    closeButton.addEventListener('click', closeNav);
+    toggleButton.addEventListener('click', toggleNav);
+    closeButton.addEventListener('click', toggleNav);
 
     for (var i = 0, len = menuItems.length; i < len; i++) {
-        menuItems[i].addEventListener('click', closeNav);
+        menuItems[i].addEventListener('click', toggleNav);
     }
 
-    function handleTouch(e) {
-        var x = e.changedTouches[0].clientX;
-        var total = this.clientWidth;
-        var position = x - total;
-        if (position < 0) this.style.left = (x - total) + 'px'
-        else if (position >= 0) this.style.left = 0 + 'px'
-    }
-
-    function handleTouchEnd(e) {
-        var x = e.changedTouches[0].clientX;
-        var total = this.clientWidth;
-        var position = x - total;
-        this.style.left = "";
-        if (position <= -total * 0.5) { closeNav(); }
-    }
-
-    mobileNav.addEventListener('touchstart', handleTouch, false)
-    mobileNav.addEventListener('touchmove', handleTouch, false)
-    mobileNav.addEventListener('touchend', handleTouchEnd, false)
-    document.getElementById('nav__modal').addEventListener('click', closeNav, false);
+    document.getElementById('nav__modal').addEventListener('click', toggleNav, false);
 
 
-    //check to see if user is using keyboard navigation before removing blue focus outline (accessibility)
-    function handleFirstTab(e) {
-        if (e.keyCode === 9) { // the "I am a keyboard user" key
-            document.body.classList.add('user-is-tabbing');
-            window.removeEventListener('keydown', handleFirstTab);
-        }
-    }
+    // function handleTouch(e) {
+    //     var x = e.changedTouches[0].clientX;
+    //     var total = this.clientWidth;
+    //     var position = x - total;
+    //     if (position < 0) this.style.left = (x - total) + 'px'
+    //     else if (position >= 0) this.style.left = 0 + 'px'
+    // }
 
-    window.addEventListener('keydown', handleFirstTab);
+    // function handleTouchEnd(e) {
+    //     var x = e.changedTouches[0].clientX;
+    //     var total = this.clientWidth;
+    //     var position = x - total;
+    //     this.style.left = "";
+    //     if (position <= -total * 0.5) { closeNav(); }
+    // }
 
+    // mobileNav.addEventListener('touchstart', handleTouch, false)
+    // mobileNav.addEventListener('touchmove', handleTouch, false)
+    // mobileNav.addEventListener('touchend', handleTouchEnd, false)
 
 
 }, false);
